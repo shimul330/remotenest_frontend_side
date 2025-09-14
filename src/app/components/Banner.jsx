c6282a16ed251b8d9ad84ccb9694d6b37e6cc143
developerTamanna <astamanna57@gmail.com>
2025-09-14 11:10:37 +0600

    structure build and basic setup

---
+++ src/app/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Banner = () => {
+  return (
+    <div>
+      <h1 className='text-5xl font-bold underline text-red-500'>Banner</h1>
+
+    </div>
+  );
+};
+
+export default Banner;
