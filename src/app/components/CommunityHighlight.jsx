8144a6b21ead651767188510f57fd6c180bb407b
unknown <mdfaruk01720875872>
2025-09-14 23:25:31 +0600

    project setup

---
+++ src/app/components/CommunityHighlight.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CommunityHighlight = () => {
+    return (
+        <div>
+            <h1>this is CommunityHighlight</h1>
+        </div>
+    );
+};
+
+export default CommunityHighlight;
