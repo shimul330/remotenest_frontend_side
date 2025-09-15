f2e0f67769fc20f4155e18832720c47cc90c7f98
Shimul dev nath <shimuldevnath330@gmail.com>
2025-09-15 22:49:32 +0600

    added a

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,14 @@
 import Image from "next/image";
 import Banner from "./components/Banner";
-<<<<<<< HEAD
 import HowItWorks from "./components/HowItWorks";
-=======
 import CommunityHighlight from "./components/CommunityHighlight";
->>>>>>> upstream/main
 
 export default function Home() {
   return (
     <div>
-    <Banner></Banner>
-<<<<<<< HEAD
-    <HowItWorks/>
-=======
-    <CommunityHighlight></CommunityHighlight>
->>>>>>> upstream/main
-   </div>
+      <Banner />
+      <HowItWorks />
+      <CommunityHighlight />
+    </div>
   );
 }
